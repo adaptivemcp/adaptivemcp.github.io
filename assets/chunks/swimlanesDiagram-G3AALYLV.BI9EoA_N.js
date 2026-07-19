d8b13ee5bb2060fd8f32b54b3bdd31a0f468d1d7
kemalelmizan <kemalelmizan@gmail.com>
2026-07-19 11:17:41 +0700

    Updates

---
+++ assets/chunks/swimlanesDiagram-G3AALYLV.BI9EoA_N.js
@@ -0,0 +1,8 @@
+import{c as r,s as e}from"./flowDiagram-23GEKE2U.D_rxCM4Y.js";import{_ as a}from"../app.BIZ8NPYA.js";import"./chunk-5VM5RSS4.Cvc8BzJa.js";import"./chunk-XXDRQBXY.DB2QRyfx.js";import"./chunk-VR4S4FIN.bTK3nsDa.js";import"./chunk-32BRIVSS.BpuhjOao.js";import"./channel.Db4hjQbh.js";import"./framework.C43iKuuU.js";import"./theme.CWU8mots.js";var o=a(t=>`${e(t)}
+  .swimlane.cluster rect {
+    stroke: ${t.clusterBorder} !important;
+  }
+  [data-look="neo"].cluster rect {
+    filter: none;
+  }
+`,"getStyles"),m=o,y=r({defaultLayout:"swimlane",styles:m});export{y as diagram};
